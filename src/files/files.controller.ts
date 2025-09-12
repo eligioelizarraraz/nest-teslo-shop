5de34a00adc728f6f27313eb2b922d0fa49af2f7
Eligio Elizarraraz <eligio.elizarraraz@prouniversitaria.org>
2025-09-12 09:04:59 -0600

    feat: se sirven los archivos de manera controlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,10 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -9,10 +12,20 @@ import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { fileNamer, fileFilter } from './helpers';
+import type { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -31,8 +44,10 @@ export class FilesController {
     if (!file)
       throw new BadRequestException('Asegúrate que el archivo es una imagen');
 
+    const secureUrl = `${file.filename}`;
+
     return {
-      fieldName: file.originalname,
+      secureUrl,
     };
   }
 }
