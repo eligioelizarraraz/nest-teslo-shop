287908cda364801b625f6892e46d60a6ada28ef8
Eligio Elizarraraz <eligio.elizarraraz@prouniversitaria.org>
2025-09-13 09:15:55 -0600

    chore: se instala bcrypt para cifrado de contraseñas y se omite el enviado de contraseña

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,6 +5,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
@@ -18,8 +19,14 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+
       await this.userRepository.save(user);
+      delete (user as any).password;
 
       return user;
     } catch (error) {
