5dc95b3432cb961666ccc981e009dc2d07f1db95
Eligio Elizarraraz <eligio.elizarraraz@prouniversitaria.org>
2025-09-14 12:49:11 -0600

    feat: añadir usuarios y vincularlos a los productos mediante SEED

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,27 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from '../products/products.service';
 import { initialData } from './data/seed-data';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SeedService {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(
+    private readonly productsService: ProductsService,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
 
   async runSeed() {
-    await this.insertNewProducts();
+    await this.deleteTables();
+
+    const adminUser = await this.insertUsers();
+    await this.insertNewProducts(adminUser);
+
     return 'SEED executed';
   }
 
-  private async insertNewProducts() {
+  private async deleteTables() {
     await this.productsService.deleteAllProducts();
 
+    const queryBuilder = this.usersRepository.createQueryBuilder();
+    await queryBuilder.delete().where({}).execute();
+  }
+
+  private async insertUsers() {
+    const seedUsers = initialData.users;
+
+    const users: User[] = [];
+    seedUsers.forEach((user) => {
+      users.push(this.usersRepository.create(user));
+    });
+
+    const dbUsers = await this.usersRepository.save(seedUsers);
+    // console.log({ seedUsers, users, dbUsers });
+
+    return dbUsers[0];
+  }
+
+  private async insertNewProducts(user: User) {
     const products = initialData.products;
 
     const insertPromises: Promise<any>[] = [];
 
-    // products.forEach((product) => {
-    //   // Me funciona porque parece como el CreateProductDto
-    //   insertPromises.push(this.productsService.create(product));
-    // });
+    products.forEach((product) => {
+      // Me funciona porque parece como el CreateProductDto
+      insertPromises.push(this.productsService.create(product, user));
+    });
 
     const results = await Promise.all(insertPromises);
 
