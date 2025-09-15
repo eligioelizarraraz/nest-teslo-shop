4aba57203602636fae85fedda243afa962ed6be6
Eligio Elizarraraz <eligio.elizarraraz@prouniversitaria.org>
2025-09-15 14:11:42 -0600

    feat: se implementa lógica para solamente permitir una conexión al socket por usuario

--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -5,6 +5,7 @@ import { User } from 'src/auth/entities/user.entity';
 import { Repository } from 'typeorm';
 
 // El ID del cliente referido al Socket
+// Para la implementación de una sola conexión, asumo que solo quiero tener una instancia de socketID por usuario
 interface ConnectedClients {
   [id: string]: {
     socket: Socket;
@@ -26,6 +27,8 @@ export class MessagesWsService {
     if (!user) throw new Error('User not found');
     if (!user.isActive) throw new Error('User not active');
 
+    this.checkUserConnection(user);
+
     this.connectedClients[client.id] = {
       socket: client,
       user: user,
@@ -43,5 +46,16 @@ export class MessagesWsService {
 
   getUserFullName(socketId: string) {
     return this.connectedClients[socketId].user.fullName;
+  }
+
+  private checkUserConnection(user: User) {
+    for (const clientId of Object.keys(this.connectedClients)) {
+      const connectedClient = this.connectedClients[clientId];
+
+      if (connectedClient.user.id === user.id) {
+        connectedClient.socket.disconnect();
+        break;
+      }
+    }
   }
 }
