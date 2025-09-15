3a6a836265c2488a23ef402f37e8af7239bdb6e4
Eligio Elizarraraz <eligio.elizarraraz@prouniversitaria.org>
2025-09-14 23:29:37 -0600

    feat: documentación de DTOs con decorador ApiProperty

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -10,41 +11,54 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product Title (unique)',
+    nullable: false,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty()
   @IsArray()
   @IsString({ each: true })
   sizes: string[];
 
+  @ApiProperty()
   @IsString()
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty()
   @IsArray()
   @IsString({ each: true })
   @IsOptional()
   tags?: string[];
 
+  @ApiProperty()
   @IsArray()
   @IsString({ each: true })
   @IsOptional()
