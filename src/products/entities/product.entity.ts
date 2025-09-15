6b20db3964aa227e112dfe9dbe650d18cfb31fa5
Eligio Elizarraraz <eligio.elizarraraz@prouniversitaria.org>
2025-09-14 23:18:02 -0600

    feat: se documentan las respuestas ApiResponse y se establecen las propiedades de respuesta mediante documentación de las entidades usando ApiProperty en cada columna

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -9,39 +9,81 @@ import {
 } from 'typeorm';
 import { ProductImage } from './product-image.entity';
 import { User } from 'src/auth/entities/user.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'products' })
 export class Product {
+  @ApiProperty({
+    example: '35e4096f-df6a-4f0f-a5ea-51089e7c7daa',
+    description: 'Product ID',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'Men’s Chill Crew Neck Sweatshirt',
+    description: 'Product Title',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty({
+    example: 75,
+    description: 'Product Price',
+    default: 0,
+  })
   @Column('float', { default: 0 })
   price: number;
 
+  @ApiProperty({
+    example:
+      'Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.',
+    description: 'Product Description',
+    default: null,
+  })
   @Column({ type: 'text', nullable: true })
   description: string;
 
+  @ApiProperty({
+    example: 'mens_chill_crew_neck_sweatshirt',
+    description: 'Product Slug',
+    uniqueItems: true,
+  })
   @Column('text', { unique: true })
   slug: string;
 
+  @ApiProperty({
+    example: 7,
+    description: 'Product Stock',
+    default: 0,
+  })
   @Column('int', { default: 0 })
   stock: number;
 
+  @ApiProperty({
+    example: ['XS', 'S', 'M', 'L', 'XL', 'XXL'],
+    description: 'Product Sizes',
+  })
   @Column('text', { array: true })
   sizes: string[];
 
+  @ApiProperty({
+    example: 'men',
+    description: 'Product Gender',
+  })
   @Column('text')
   gender: string;
 
+  @ApiProperty()
   @Column('text', { array: true, default: [] })
   tags: string[];
 
   // images
+  @ApiProperty()
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,
