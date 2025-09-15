4cee4fc154d928d4f5609917ef6ab7c418d8756b
Eligio Elizarraraz <eligio.elizarraraz@prouniversitaria.org>
2025-09-15 12:31:04 -0600

    feat: se configura emisión y escucha del cliente desde el servidor y se integra DTO para la llegada de información

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,20 +1,32 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dto/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer()
+  webSocketServer: Server;
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
     // console.log('Cliente conectado: ', client.id);
     this.messagesWsService.registerClient(client);
+
+    // De esta manera le enviamos la comunicación a nuestro cliente, pero debemos esperar a que la reciba (escuche)
+    this.webSocketServer.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
 
     // console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
@@ -22,5 +34,19 @@ export class MessagesWsGateway
   handleDisconnect(client: Socket) {
     // console.log('Cliente desconectado: ', client.id);
     this.messagesWsService.removeClient(client.id);
+
+    this.webSocketServer.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(
+    client: Socket,
+    // DTO creado de acuerdo a lo que se recibirá
+    payload: NewMessageDto,
+  ) {
+    console.log(client.id, payload);
   }
 }
